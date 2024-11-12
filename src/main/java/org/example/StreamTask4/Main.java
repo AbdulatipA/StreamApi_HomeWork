4d30dd8dd29f5d0f631970af832cfbe2e215ad84
AbdulatipA <ataev0461@gmail.com>
2024-11-12 21:59:46 +0300

    commit_3

--- src/main/java/org/example/StreamTask4/Main.java
+++ src/main/java/org/example/StreamTask4/Main.java
@@ -1,9 +1,6 @@
 package org.example.StreamTask4;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -33,36 +30,33 @@ public class Main {
 
         //3.Удалить дубликаты из списка строк и получить результат в виде массива строк.
         List<String> dublicateList = new ArrayList<>();
-        dublicateList.add("John");
-        dublicateList.add("Yana");
-        dublicateList.add("Jack");
-        dublicateList.add("John");
-        dublicateList.add("Jack");
-        dublicateList.add("Anton");
-//        System.out.println(dublicateList);
+        Collections.addAll(dublicateList, "John", "Yana", "Jack", "John", "Jack", "Anton");
+//        dublicateList.add("John");
+//        dublicateList.add("Yana");
+//        dublicateList.add("Jack");
+//        dublicateList.add("John");
+//        dublicateList.add("Jack");
+//        dublicateList.add("Anton");
+        System.out.println(dublicateList);
 
         List<String> noDublicateList = dublicateList.stream()
                 .distinct()
                 .toList();
-//        System.out.println(noDublicateList);
+        System.out.println(noDublicateList);
 
 
         //4.Разделить список чисел на две группы: четные и нечетные, и получить мапу.
         List<Integer> numbers = new ArrayList<>();
-        numbers.add(1);
-        numbers.add(2);
-        numbers.add(3);
-        numbers.add(4);
-        numbers.add(5);
-        numbers.add(6);
+        Collections.addAll(numbers, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
         Map<Boolean, List<Integer>> groupNumbers = numbers.stream()
                 .collect(Collectors.groupingBy(e -> e % 2 == 0));
 
         for (Map.Entry<Boolean, List<Integer>> entry : groupNumbers.entrySet()) {
             List<Integer> list = entry.getValue();
-//            System.out.println(entry.getKey());
+            System.out.println(entry.getKey());
 
-//            list.forEach(System.out::println);
+            list.forEach(System.out::println);
         }
     }
 }
